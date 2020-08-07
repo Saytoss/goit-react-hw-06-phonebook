aad25ae0dadb510c9714dd1a7c18a5a9cc9f70dd
Saytos <korana6277@gmail.com>
2020-08-07 14:06:30 +0300

    hw06_fix

---
+++ build/precache-manifest.2c0de315873f89b9401564532b8bf302.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf3a618187c917bfd7a0e38834943c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a38306a2760e56cbf35c",
+    "url": "./static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "11c5bf51d20fdbd3a1e0",
+    "url": "./static/css/main.e58d094e.chunk.css"
+  },
+  {
+    "revision": "a38306a2760e56cbf35c",
+    "url": "./static/js/2.5dafd36c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.5dafd36c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c5bf51d20fdbd3a1e0",
+    "url": "./static/js/main.3df76082.chunk.js"
+  },
+  {
+    "revision": "8bf42de62689b0eddcee",
+    "url": "./static/js/runtime-main.f501d800.js"
+  }
+]);
